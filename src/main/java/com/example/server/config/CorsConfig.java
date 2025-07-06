a37392a3c29980345b7907a0c3a80a5018ce5caf
Alla Trivikram <allatrivikram@gmail.com>
2025-07-06 11:39:12 +0530

    Update CorsConfig.java

--- src/main/java/com/example/server/config/CorsConfig.java
+++ src/main/java/com/example/server/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("https://vkstore2025.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
