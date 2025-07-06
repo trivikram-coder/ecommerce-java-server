ed6f597ee590ddd82a14609f66fa0a56952474c7
Alla Trivikram <allatrivikram@gmail.com>
2025-07-06 10:37:46 +0530

    Update SecurityConfig.java

--- src/main/java/com/example/server/config/SecurityConfig.java
+++ src/main/java/com/example/server/config/SecurityConfig.java
@@ -1,48 +1,46 @@
 package com.example.server.config;
 
 import com.example.server.Accounts.AccountService;
+import com.example.server.Filter.JwtFilter;
 import com.example.server.Utility.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
+
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 @Configuration
 public class SecurityConfig {
-
     @Autowired
     private AccountService accountService;
-
     @Autowired
-    @Lazy
+
     private JwtUtil jwtUtil;
     @Autowired
-   private JwtFilter jwtFilter;
+    private JwtFilter jwtFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-                .csrf(csrf -> csrf.disable())
+        return http.
+                csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                            "/auth/signin",
-                            "/auth/signup",
-                            "/cart/**",
-                            "/products/**"
-                        ).permitAll()
+                        .requestMatchers("/auth/**").permitAll()     // 👈 allow public access
+                        .requestMatchers("/cart/**", "/products/**").permitAll()
                         .anyRequest().authenticated()
+
                 )
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
-
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration cofig) throws Exception{
+        return cofig.getAuthenticationManager();
     }
+
 }
