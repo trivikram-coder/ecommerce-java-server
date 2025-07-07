d01bc48850b3c87f29df9064bb3f6272a66b6697
vikram-42-alla <allatrivikram@gmail.com>
2025-07-07 12:31:23 +0530

    commit

--- src/main/java/com/example/server/config/CorsConfig.java
+++ src/main/java/com/example/server/config/CorsConfig.java
@@ -10,7 +10,8 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://vkstore2025.netlify.app/","https://vkstoreadmin.netlify.app/")
+                .allowedOrigins("https://vkstore2025.netlify.app/")
+                .allowedOrigins("https://vkstoreadmin.netlify.app/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
