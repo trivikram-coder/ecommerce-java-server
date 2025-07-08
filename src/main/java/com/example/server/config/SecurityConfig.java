930dd74d1846856b0ad517f904d0e4ffcca91f5a
vikram-42-alla <allatrivikram@gmail.com>
2025-07-08 05:58:32 +0530

    commit

--- src/main/java/com/example/server/config/SecurityConfig.java
+++ src/main/java/com/example/server/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,6 +29,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.
                 csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/auth/signup","/auth/signin","/admin/signin","/admin/signup").permitAll()
                        
