b21f2e277dbc8608d46ed3ce43d9f8cc44980b6c
vikram-42-alla <allatrivikram@gmail.com>
2025-10-05 11:25:47 +0530

    commit

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -2,7 +2,8 @@ package com.example.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cache.annotation.EnableCaching;
+@EnableCaching
 @SpringBootApplication
 public class ServerApplication {
 
